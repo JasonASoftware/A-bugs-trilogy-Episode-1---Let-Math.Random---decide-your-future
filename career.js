958f5f2ef19137418ccb443f9629cab0379da30d
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-14 20:07:06 -0500

    update career file and added code

--- career.js
+++ career.js
@@ -1,3 +1,18 @@
+//error code 
+// function {
+// 	var : Math.random()
+// 		if (career <= 0.32) {
+// 			return = FrontEnd Developer
+// 		 else if (career <= 0.65) 
+// 			return : BackEnd Developer,
+// 		} else {
+// 			return 'Full-Stack Developer'
+// 		}
+
+
+// yourFutureCareer();
+
+//fixed code 
 function yourFutureCareer(){
 	var career =  Math.random();
 		if (career <= 0.32) {
